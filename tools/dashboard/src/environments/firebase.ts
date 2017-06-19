363fce0755563e81acdd8fa00dab2426e71cc8bb
Paul Gschwendtner <paulgschwendtner@gmail.com>
2017-06-19 18:30:17 +0200

    build: firebase integration for dashboard (#5182)
    * build: firebase integration for dashboard
    
    * As part of the initial dashboard setup, this installs Angular Fire and Angular Material in the dashboard.
    * For testing purposes the list of all payload results will be printed right now.
    
    * Address feedback

---
+++ tools/dashboard/src/environments/firebase.ts
@@ -0,0 +1,7 @@
+/** Shared Firebase configuration that will be used in every environment. */
+export const firebaseConfig = {
+  apiKey: '***REDACTED***',
+  authDomain: 'material2-board.firebaseapp.com',
+  databaseURL: 'https://material2-board.firebaseio.com',
+  projectId: 'material2-board'
+};
