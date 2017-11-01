8f3c51c6f034d4a12f363b6ee1f775aa1cb43f3c
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2017-11-01 19:03:57 +0100

    feat: angular 5 support (#6018)

--- tools/gulp/util/firebase.ts
+++ tools/gulp/util/firebase.ts
@@ -19,7 +19,7 @@ export function openFirebaseDashboardApp() {
       client_email: '***REDACTED-EMAIL***',
       // In Travis CI the private key will be incorrect because the line-breaks are escaped.
       // The line-breaks need to persist in the service account private key.
-      private_key: decode(process.env['MATERIAL2_BOARD_FIREBASE_SERVICE_KEY'])
+      private_key: decode(process.env['MATERIAL2_BOARD_FIREBASE_SERVICE_KEY']!)
     }),
   });
 }
@@ -38,7 +38,7 @@ export function openScreenshotsBucket() {
     projectId: 'material2-screenshots',
     credentials: {
       client_email: '***REDACTED-EMAIL***',
-      private_key: decode(process.env['MATERIAL2_SCREENSHOT_FIREBASE_KEY'])
+      private_key: decode(process.env['MATERIAL2_SCREENSHOT_FIREBASE_KEY']!)
     },
   });
 
