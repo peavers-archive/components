245d9f50af7c8d597879d1173cc6b0ac6f625196
Paul Gschwendtner <paulgschwendtner@gmail.com>
2018-11-07 18:04:16 +0000

    build: remove dashboard (#14023)
    * Since we going to move from Travis to CircleCI we don't want to use the dashboard anymore. Since nobody has been
    looking at the dashboard frequently, it just makes things more complicated and additionally we will be using
    the Github robot in order to keep track of size changes.

--- tools/dashboard/src/environments/firebase.ts
+++
@@ -1,7 +0,0 @@
-/** Shared Firebase configuration that will be used in every environment. */
-export const firebaseConfig = {
-  apiKey: '***REDACTED***',
-  authDomain: 'material2-board.firebaseapp.com',
-  databaseURL: 'https://material2-board.firebaseio.com',
-  projectId: 'material2-board'
-};
